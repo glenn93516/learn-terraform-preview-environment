da3bc0b7df894ff30d515673fad4e20d4afd4115
김민혁 <kminheuk@gmail.com>
2022-02-22 23:58:47 +0900

    Fix Vercel project name and TFC organization name

--- preview-env/main.tf
+++ preview-env/main.tf
@@ -6,7 +6,7 @@ data "terraform_remote_state" "shared" {
   backend = "remote"
 
   config = {
-    organization = "hashicorp-training"
+    organization = "glenn_practice"
     workspaces = {
       name = "hcup-be-shared"
     }
